95b3639506d6ad60a4f83c1343c38f7f47bce526
maxpogo <maxim.pogodaev.93@mail.ru>
2023-03-21 16:39:13 +0300

    Add last fail

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,12 @@
 public class Main {
     public static void main(String[] args) {
         int check = 200;
-        int replenishment1 = 200;
         int replenishment2 = 1400;
         int bonus1 = 1;
         int bonus0 = 0;
         boolean more1000 = true;
 
-        if (more1000) {
-            bonus1 = 1;
-        } else {
-            bonus1 = 0;
-        }
-        int bonus3 = replenishment1;
 
-        if (replenishment1 < 1000) {
-            bonus3 = 0;
-        }
-        System.out.println("Итоговые бонусные мили: " + bonus3 );
 
         if (more1000) {
             bonus1 = 1;
